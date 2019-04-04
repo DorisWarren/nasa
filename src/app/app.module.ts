cf7d7ba78bcebbea178e5608e64d6ba5649af3ae
doris warren and kohichi horii <doris@>
2019-04-04 13:26:58 -0700

    retrieving data from nasa api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RoverFormComponent } from './rover-form/rover-form.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoverFormComponent,
+    PhotoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
